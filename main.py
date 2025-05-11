e9b3b0cf9a4e370dc769df4860b1e612c96f8a4a
sakzk <s1911490@klis.tsukuba.ac.jp>
2025-05-11 21:24:10 +0900

    ADD: count appearance of each character in a document

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import *
+from stats import get_word_count, get_character_count
 
 def main():
     path_to_book_file = "./books/frankenstein.txt"
@@ -6,6 +6,8 @@ def main():
     # print(file_content)
     word_count = get_word_count(file_content)
     print(f"{word_count} words found in the document")
+    character_to_count = get_character_count(file_content) # TODO: 命名 単数形_to_単数形 でいいのか？
+    print(f"unsorted mapping: {character_to_count}")
 
 def get_book_text(path_to_book_file)-> str:
     with open(path_to_book_file) as f:
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@
 def get_word_count(file_content: str) -> int:
     # split() のセパレータの理解は怪しい。
     return len(file_content.split())
+
+def get_character_count(file_content: str) -> int:
+    character_to_count = {}
+    # file_content.lower() # NOTE: こいつは、file_content を上書きしない！
+    for char in file_content.lower():
+        if char not in character_to_count:
+            character_to_count[char] = 0
+        character_to_count[char] += 1
+    return character_to_count
