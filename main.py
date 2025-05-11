eae4d8b6e341e448bc8b10ba64a6b05d15cc59f0
sakzk <s1911490@klis.tsukuba.ac.jp>
2025-05-11 22:19:47 +0900

    ADD: report section separator for visual improvement

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from stats import get_word_count, get_character_count
 
 def main():
-    path_to_book_file = "./books/frankenstein.txt"
+    BOOK_TITLE = "frankenstein"
+
+    print("============ BOOKBOT ============")
+    path_to_book_file = f"./books/{BOOK_TITLE}.txt"
     file_content = get_book_text(path_to_book_file)
     # print(file_content)
+    print(f"Analyzing book found at books/{BOOK_TITLE}.txt...")
+
+    print("----------- Word Count ---------- ")
     word_count = get_word_count(file_content)
-    print(f"{word_count} words found in the document")
+    print(f"{word_count} words found in the document") #TODO 2
+
+    print("--------- Character Count ------- ")
     character_to_count = get_character_count(file_content) # TODO: 命名 単数形_to_単数形 でいいのか？
-    print(f"unsorted mapping: {character_to_count}")
+    # print(f"unsorted mapping: {character_to_count}")
+    prity_print_dict(character_to_count)
+
+    print("============= END =============== ")
+    
+def prity_print_dict(character_to_count: dict) -> None:
+    # TODO:
+    return 
 
 def get_book_text(path_to_book_file)-> str:
     with open(path_to_book_file) as f:
         file_content = f.read()
     return file_content
-    return None # with ブロックの仕様を理解していない。エラーハンドリングで処理するべき。
 
 main()
